652bd8a24a66d56adc205d90a4c1cf6c9dd562c3
Pascal Verkade <pascal_verkade@hotmail.com>
2021-10-30 16:40:21 +0200

    finished basic modelling

--- Data/CSEDMDataChallenge-main/test_Thimo.py
+++ Data/CSEDMDataChallenge-main/test_Thimo.py
@@ -6,21 +6,25 @@ import numpy as np
 
 class Student:
 
-    def __init__(self, problem_dic, student_id, student_skills_array):
+    def __init__(self, problem_dic, student_id, student_skills_array, problem_attempts = {}, student_w=0):
         self.student_id = student_id
         self.problems = problem_dic
         self.student_skills = student_skills_array
+        self.student_weight = student_w
+        self.problem_attempts = problem_attempts
 
 
 semester = 'S19'
 BASE_PATH = os.path.join('..', 'Release', semester)
 TRAIN_PATH = os.path.join(BASE_PATH, 'Train')
 TEST_PATH = os.path.join(BASE_PATH, 'Test')
-MAIN_PATH = os.path.join(BASE_PATH, r'Test\Data')
+MAIN_PATH = os.path.join(BASE_PATH, r'Train\Data')
 
 main = ProgSnap2Dataset(os.path.join(MAIN_PATH, 'MainTable'))
 train_ps2 = ProgSnap2Dataset(os.path.join(TRAIN_PATH, '..'))
 early_train = pd.read_csv(os.path.join(TRAIN_PATH, 'early.csv'))
+early_test = pd.read_csv(os.path.join(TRAIN_PATH, 'late.csv'))
+
 
 META_PROBLEM_FILE = r"2nd CSEDM Data Challenge - Problem Prompts _ Concepts Used.xlsx"
 META_PROBLEM_PATH = os.path.join(BASE_PATH, META_PROBLEM_FILE)
@@ -31,41 +35,106 @@ meta_problem = meta_problem.drop(["AssignmentID", 'Requirement'], axis=1)
 meta_problem_subjects = meta_problem.drop(["ProblemID"], axis=1)
 
 
+#for attempts per problem
+main_table = main.get_main_table()
+student_problem_codestate = main_table[['SubjectID', 'ProblemID', 'CodeStateID' ]]
+problem_student_table = student_problem_codestate.groupby(["ProblemID", "SubjectID"])["CodeStateID"].unique().apply(list).to_dict()
+
+
+#for avg attempts
+    
 def initialise_students(student_dataframe):
     person_ids = student_dataframe["SubjectID"].unique()
     subjects = meta_problem_subjects.iloc[0].to_numpy()
     student_list = []
+    
     for person_id in person_ids:
         row_person = student_dataframe[student_dataframe["SubjectID"] == person_id]
         problems = dict(zip(row_person.ProblemID, row_person.Label))
         student_skills = np.zeros(len(subjects))
-        new_student = Student(problem_dic=problems, student_id=person_id, student_skills_array=student_skills)
+        problem_attempts = {}
+        for (problem, person_id) in problem_student_table:
+            problem_attempts[problem] = len(problem_student_table[(problem, person_id)])
+        new_student = Student(problem_dic=problems, student_id=person_id, student_skills_array=student_skills, problem_attempts=problem_attempts)
         student_list.append(new_student)
-        return student_list
+        
+    return student_list
 
+def get_meta_data():
+    
+    problem_ids_train = early_train["ProblemID"].unique().tolist()
+    problem_ids_test = early_test["ProblemID"].unique().tolist()
+    
+    problem_ids = problem_ids_train + problem_ids_test
+
+    aa = {}
+    sd = {}
+    pw = {}
+    no_concepts = {}
+    
+    #calculate total concepts accross all problems
+    #for problem in meta_problem_subjects:
+    #    no_concepts[problem] = sum(meta_problem_subjects[problem])
+        
+    for problem_id in problem_ids:      
+        no_concepts[problem_id] = meta_problem.loc[meta_problem['ProblemID'] == problem_id].drop(['ProblemID'], axis=1).to_numpy()
+        attempts = []
+        attempts_student = {}
+        for x,y in problem_student_table:
+            if x==problem_id:
+                attempts_student[y] = len(problem_student_table[(problem_id, y)])
+                attempts = attempts + problem_student_table[(problem_id, y)]
+        aa[problem_id] = len(attempts)/len(early_train["SubjectID"].unique())
+          
+        standaardafwijking = np.std(list(attempts_student.values()))
+        sd[problem_id] = standaardafwijking
+        
+        pw[problem_id] = aa[problem_id] + len(no_concepts[problem_id])
+                                              
+    return aa, sd, pw, no_concepts
 
 student_list = initialise_students(early_train)
+average_attempts, sd_average_attempts, problem_weights, problem_concepts = get_meta_data()
 
 
-def update_model(student_skills, problem):
+def update_model(student, problem):
     problem_row = meta_problem[meta_problem["ProblemID"] == problem]
+    
+    #to array
     problem_row = problem_row.drop(["ProblemID"], axis=1).to_numpy()
     problem_row = problem_row.flatten()
-
-    student_skills += problem_row
-
+    
+    student_weight_old = student.student_weight
+    student.student_weight = student.student_weight + problem_weights[problem]
+    
+    for i, ability in enumerate(problem_row):
+        if ability == 1:
+            score = (student.problem_attempts[problem] / (average_attempts[problem] + sd_average_attempts[problem] )) * student.problems[problem]
+            student.student_skills[i] = ((student.student_skills[i] * student_weight_old) + (score * problem_weights[problem])) / np.where(student.problems[problem] > 0, student.student_weight, 1)
 
 def model_abilities(student):
     for problem in student.problems:
-        if student.problems[problem]:
-            update_model(student.student_skills, problem)
-        else:
-            continue
+            update_model(student, problem)
 
-print(student_list[0].student_skills)
+
+print(student_list[50].student_skills)
 for student in student_list:
     model_abilities(student)
-print(student_list[0].student_skills)
+print(student_list[50].student_skills)
+
+
+
+
+main_table = main.get_main_table()
+student_problem_codestate = main_table[['SubjectID', 'ProblemID', 'CodeStateID' ]]
+
+test = student_problem_codestate.groupby(["ProblemID", "SubjectID"])["CodeStateID"].apply(list).to_dict()
+
+
+
+
+
+
 
 
 """"
