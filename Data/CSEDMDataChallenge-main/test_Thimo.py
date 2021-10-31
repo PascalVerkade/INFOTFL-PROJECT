38c7695ae46ad48cc1edd8bc9fd25eede480f362
Pascal Verkade <pascal_verkade@hotmail.com>
2021-10-31 21:16:33 +0100

    changed formulas and added needed functionality and data for this, fixed issue with generation student list

--- Data/CSEDMDataChallenge-main/test_Thimo.py
+++ Data/CSEDMDataChallenge-main/test_Thimo.py
@@ -2,17 +2,29 @@ import os
 import pandas as pd
 from ProgSnap2 import ProgSnap2Dataset
 import numpy as np
+import scipy.stats
+
 
 
 class Student:
 
-    def __init__(self, problem_dic, student_id, student_skills_array, problem_attempts = {}, student_w=0):
+    def __init__(self, problem_dic, student_id, student_skills_array, problem_attempts = {}, problem_scores = {}, student_w={}):
         self.student_id = student_id
         self.problems = problem_dic
         self.student_skills = student_skills_array
-        self.student_weight = student_w
+        
+        self.student_weight = {}
+        for ability in meta_problem_subjects:
+            self.student_weight[ability] = 0
+        
         self.problem_attempts = problem_attempts
+        self.problem_scores = problem_scores
 
+    def get_by_id_from_list(id_student, list_of_students):
+        for x in list_of_students:
+            if x.student_id == id_student:
+                return x
+        return None
 
 semester = 'S19'
 BASE_PATH = os.path.join('..', 'Release', semester)
@@ -53,14 +65,15 @@ def initialise_students(student_dataframe):
         problems = dict(zip(row_person.ProblemID, row_person.Label))
         student_skills = np.zeros(len(subjects))
         problem_attempts = {}
-        for (problem, person_id) in problem_student_table:
-            problem_attempts[problem] = len(problem_student_table[(problem, person_id)])
+        for (problem, stud_id) in problem_student_table:
+            if person_id == stud_id:
+                problem_attempts[problem] = len(problem_student_table[(problem, person_id)])
         new_student = Student(problem_dic=problems, student_id=person_id, student_skills_array=student_skills, problem_attempts=problem_attempts)
         student_list.append(new_student)
         
     return student_list
 
-def get_meta_data():
+def get_meta_data(students):
     
     problem_ids_train = early_train["ProblemID"].unique().tolist()
     problem_ids_test = early_test["ProblemID"].unique().tolist()
@@ -72,55 +85,70 @@ def get_meta_data():
     pw = {}
     no_concepts = {}
     
-    #calculate total concepts accross all problems
-    #for problem in meta_problem_subjects:
-    #    no_concepts[problem] = sum(meta_problem_subjects[problem])
-        
+   
     for problem_id in problem_ids:      
         no_concepts[problem_id] = meta_problem.loc[meta_problem['ProblemID'] == problem_id].drop(['ProblemID'], axis=1).to_numpy()
         attempts = []
         attempts_student = {}
+        
+       
         for x,y in problem_student_table:
             if x==problem_id:
-                attempts_student[y] = len(problem_student_table[(problem_id, y)])
+                student = Student.get_by_id_from_list(y, students)
+                attempts_student[student] = len(problem_student_table[(problem_id, y)])
                 attempts = attempts + problem_student_table[(problem_id, y)]
+            
         aa[problem_id] = len(attempts)/len(early_train["SubjectID"].unique())
           
         standaardafwijking = np.std(list(attempts_student.values()))
         sd[problem_id] = standaardafwijking
+        
+        z_scores = scipy.stats.zscore(list(attempts_student.values()))
+        #reversing z-scores and mapping to distribution
+        scores = scipy.stats.norm.cdf([0-x for x in z_scores])
+        
+        #adding problem scores to students
+        student_scores = zip(attempts_student.keys(), scores)
+        for student,score in student_scores:
+            student.problem_scores[problem_id] = score
         
         pw[problem_id] = aa[problem_id] + len(no_concepts[problem_id])
                                               
     return aa, sd, pw, no_concepts
 
 student_list = initialise_students(early_train)
-average_attempts, sd_average_attempts, problem_weights, problem_concepts = get_meta_data()
+average_attempts, sd_average_attempts, problem_weights, problem_concepts = get_meta_data(student_list)
 
 
 def update_model(student, problem):
-    problem_row = meta_problem[meta_problem["ProblemID"] == problem]
+    problem_row_name = meta_problem[meta_problem["ProblemID"] == problem]
+    problem_row_name = problem_row_name.drop(['ProblemID'], axis=1)
     
     #to array
-    problem_row = problem_row.drop(["ProblemID"], axis=1).to_numpy()
-    problem_row = problem_row.flatten()
+    #problem_row_name = problem_row_name.drop(["ProblemID"], axis=1).to_numpy()
+    #problem_row = problem_row_name.flatten()
     
-    student_weight_old = student.student_weight
-    student.student_weight = student.student_weight + problem_weights[problem]
+    #student_weight_old = student.student_weight
+    #student.student_weight = student.student_weight + problem_weights[problem]
     
-    for i, ability in enumerate(problem_row):
-        if ability == 1:
-            score = (student.problem_attempts[problem] / (average_attempts[problem] + sd_average_attempts[problem] )) * student.problems[problem]
-            student.student_skills[i] = ((student.student_skills[i] * student_weight_old) + (score * problem_weights[problem])) / np.where(student.problems[problem] > 0, student.student_weight, 1)
+    for i, ability in enumerate(problem_row_name):
+        if problem_row_name.iloc[0][ability] == 1:
+            student_weight_old = student.student_weight[ability]
+            student.student_weight[ability] = student.student_weight[ability] + problem_weights[problem]
+            #score = (student.problem_attempts[problem] / (average_attempts[problem] + sd_average_attempts[problem] )) * student.problems[problem]
+            score = student.problem_scores[problem] * student.problems[problem]
+            #if student.problems[problem]:
+            student.student_skills[i] = ((student.student_skills[i] * student_weight_old) + (score * problem_weights[problem])) / student.student_weight[ability]
 
 def model_abilities(student):
     for problem in student.problems:
             update_model(student, problem)
 
 
-print(student_list[50].student_skills)
+print(student_list[120].student_skills)
 for student in student_list:
     model_abilities(student)
-print(student_list[50].student_skills)
+print(student_list[120].student_skills)
 
 
 
@@ -132,9 +160,13 @@ test = student_problem_codestate.groupby(["ProblemID", "SubjectID"])["CodeStateI
 
 
 
+ #calculate total concepts accross all problems
+total_concepts = {}
+for problem in meta_problem_subjects:
+    total_concepts[problem] = sum(meta_problem_subjects[problem])
+print(total_concepts)
 
-
-
+test = scipy.stats.zscore([3, 6])
 
 
 """"
